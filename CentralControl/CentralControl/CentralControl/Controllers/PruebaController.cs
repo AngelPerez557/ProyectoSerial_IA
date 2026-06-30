c8dcdf2441ebd458191462c2917513d9dfb87924
DianyLiz <dlenamorado@uth.hn>
2026-06-29 21:45:03 -0600

    dd

--- CentralControl/CentralControl/CentralControl/Controllers/PruebaController.cs
+++ CentralControl/CentralControl/CentralControl/Controllers/PruebaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CentralControl.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,34 @@ namespace CentralControl.Controllers
 {
     public class PruebaController : Controller
     {
+        SerialControl serial { get; set; }
+
+        public PruebaController()
+        {
+            serial = new SerialControl("COM8");
+        }
         // GET: Prueba
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string op)
+        {
+            if (!string.IsNullOrEmpty(op))
+            {
+                try
+                {
+                    serial.Open();
+                    serial.Write(op);
+                    serial.Close();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
             return View();
         }
     }
